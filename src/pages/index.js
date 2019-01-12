75972e2013c745d29e6d08d235bff61762348a1f
Tammy Tian <tammyt@kencook.com>
2019-01-12 10:11:44 -0600

    Created react page!

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,3 @@
 import React from "react"
 
-export default () => <div>This is no long just "Hello World, Hello Galaxy!"</div>
+export default () => <div>This is no long just "Hello World, Hello Galaxy"</div>
