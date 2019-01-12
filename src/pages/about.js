be8220fa61cd05312b325e7ede0fd96f6519a783
Tammy Tian <tammyt@kencook.com>
2019-01-12 10:46:00 -0600

    editing pages

---
+++ src/pages/about.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import Header from '../components.js/Header'
+
+export default () => (
+    <div style={{color:`#48628c`}}>
+        <Header />
+        <h1>About</h1>
+        <p>The galaxy beyond is beyond our imagination.</p>
+    </div>
+)
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,11 @@
 import React from "react"
+import Header from '../components.js/Header'
 
-export default () => <div>This is no long just "Hello World, Hello Galaxy"</div>
+export default () => 
+<div style={{color:`#2e4466`}}>
+    <Header/>
+    <p>This is no long just "Hello World..."</p>
+    <h1>"Hello Galaxy!"</h1>
+    <img src="https://source.unsplash.com/random/400x200" alt="" />
+
+</div>
